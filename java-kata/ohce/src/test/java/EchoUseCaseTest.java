89ed32f5a2f3794dbdd6cafadebf92280a747cd7
loris-dinardo <loris.dinardo@gmail.com>
2022-03-02 11:09:21 +0100

    Echo use case rule definition + add one more test

--- java-kata/ohce/src/test/java/EchoUseCaseTest.java
+++ java-kata/ohce/src/test/java/EchoUseCaseTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EchoUseCaseTest {
+    /**
+     *  Rules of echo
+     *  1) Should return the word sent by user but in reverse order
+     */
+
     @ParameterizedTest
     @CsvSource({
             "echo, ohce",
-            "Hola!, !aloH"
+            "Hola!, !aloH",
+            "Hola amigo!, !ogima aloH"
     })
     void shouldReturnTheWorldSentByUserInReverse(
             String worldSentByUser,
