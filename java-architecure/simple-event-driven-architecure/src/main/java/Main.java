e1bce49cf021c29ba7be6d286324d84d28bb7a3a
loris-dinardo <loris.dinardo@gmail.com>
2022-03-09 14:52:07 +0100

    Modify main to allow user to set Player One and Two a name

--- java-architecure/simple-event-driven-architecure/src/main/java/Main.java
+++ java-architecure/simple-event-driven-architecure/src/main/java/Main.java
@@ -1,24 +1,24 @@
 import application.Player;
 import application.PlayerFactory;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Ping Pong game : Player One vs Player Two");
         PlayerFactory playerFactory = PlayerFactory.getDefault();
-        Player playerOne = playerFactory.createPlayerNamed("Player One");
-        Player playerTwo = playerFactory.createPlayerNamed("Player Two");
-
-        playerOne.servesForPointAgainstOpponent("Point_1", playerTwo);
-
-        /*
+        System.out.println("--------------------------");
+        System.out.println("Welcome to Ping Pong game");
+        System.out.println("--------------------------");
         Scanner in = new Scanner(System.in);
-        while (true) {
-            String command = in.nextLine();
-            if (command.equals("Stop!"))
-                break;
-        }
-        in.close();
-         */
+        System.out.println("Enter Player One name");
+        Player playerOne = playerFactory.createPlayerNamed(in.nextLine());
+        System.out.println("Enter Player Two name");
+        Player playerTwo = playerFactory.createPlayerNamed(in.nextLine());
+        System.out.println("--------------------------");
+        System.out.println("The game is about to start");
+        System.out.println("--------------------------");
+        playerOne.servesForPointAgainstOpponent("Point_1", playerTwo);
+        System.out.println("--------------------------");
         System.out.println("The game is finished! Thanks for watching");
     }
 }
