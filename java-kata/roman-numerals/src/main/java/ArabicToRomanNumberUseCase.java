006047b349a9973964d110dbf38d5db2a019fbd4
loris-dinardo <loris.dinardo@gmail.com>
2022-03-09 18:51:00 +0100

    Done all cases

--- java-kata/roman-numerals/src/main/java/ArabicToRomanNumberUseCase.java
+++ java-kata/roman-numerals/src/main/java/ArabicToRomanNumberUseCase.java
@@ -1,5 +1,31 @@
 public class ArabicToRomanNumberUseCase {
+    private enum Roman {
+        M(1000),
+        CM(900),
+        D(500),
+        CD(400),
+        C(100),
+        XC(90),
+        L(50),
+        XL(40),
+        X(10),
+        IX(9),
+        V(5),
+        IV(4),
+        I(1);
+
+        private final int value;
+
+        Roman(int value) {
+            this.value = value;
+        }
+    }
+
     public String execute(int arabicNumber) {
-        return "I";
+        for (Roman roman : Roman.values()) {
+            if (arabicNumber >= roman.value)
+                return roman.name() + execute(arabicNumber - roman.value);
+        }
+        return "";
     }
 }
--- java-kata/roman-numerals/src/test/java/ArabicToRomanNumberTest.java
+++ java-kata/roman-numerals/src/test/java/ArabicToRomanNumberTest.java
@@ -7,6 +7,21 @@ public class ArabicToRomanNumberTest {
     @ParameterizedTest
     @CsvSource({
             "1, I",
+            "3, III",
+            "4, IV",
+            "5, V",
+            "9, IX",
+            "10, X",
+            "28, XXVIII",
+            "49, XLIX",
+            "54, LIV",
+            "99, XCIX",
+            "127, CXXVII",
+            "203, CCIII",
+            "449, CDXLIX",
+            "549, DXLIX",
+            "999, CMXCIX",
+            "1999, MCMXCIX"
     })
     void shouldConvertArabicToRomanNumber(
             int arabicNumber,
