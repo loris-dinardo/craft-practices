a98502d2769ea1fa4bb2d9391e13949ce5b5463f
loris-dinardo <loris.dinardo@gmail.com>
2023-03-24 16:37:24 +0100

    Add view timeline in command + implement fs repository

--- typescript-kata/crafty/src/file-system-message-repository.ts
+++ typescript-kata/crafty/src/file-system-message-repository.ts
@@ -4,16 +4,38 @@ import {MessageRepository} from "./message.repository";
 import {Message} from "./message";
 
 export class FileSystemMessageRepository implements MessageRepository {
-    public message?: Message = undefined;
+    private readonly messagePath = path.join(__dirname, "messages.json");
 
     async save(msg: Message): Promise<void> {
+        const messages = await this.getMessages();
+        messages.push(msg);
         return fs.promises.writeFile(
-            path.join(__dirname, "message.json"),
-            JSON.stringify(msg)
+            this.messagePath,
+            JSON.stringify(messages)
         );
     }
 
-    findMessagesByAuthorId(userId: string): Promise<Message[]> {
-        return Promise.resolve([]);
+    async findMessagesByAuthorId(userId: string): Promise<Message[]> {
+        const messages = await this.getMessages();
+        return messages.filter(msg => msg.authorId === userId);
+    }
+
+    private async getMessages(): Promise<Message[]> {
+        const data = await fs.promises.readFile(
+            this.messagePath
+        );
+        const messages = JSON.parse(data.toString()) as {
+            id: string,
+            authorId: string,
+            text: string,
+            publishedAt: string
+        }[];
+
+        return messages.map(msg => ({
+            id: msg.id,
+            authorId: msg.authorId,
+            text: msg.text,
+            publishedAt: new Date(msg.publishedAt)
+        }));
     }
 }
--- typescript-kata/crafty/src/index.ts
+++ typescript-kata/crafty/src/index.ts
@@ -3,10 +3,15 @@ import {Argument, Command} from 'commander';
 import {PostMessageCommand, PostMessageUseCase} from "./post-message.use-case";
 import {RealDateProvider} from "./real-date-provider";
 import {FileSystemMessageRepository} from "./file-system-message-repository";
+import {ViewTimelineUseCase} from "./view-timeline.use-case";
 
 const messageRepository = new FileSystemMessageRepository();
 const dateProvider = new RealDateProvider();
 const postMessageUseCase = new PostMessageUseCase(
+    messageRepository, dateProvider
+);
+
+const viewTimelineUseCase = new ViewTimelineUseCase(
     messageRepository, dateProvider
 );
 const program = new Command();
@@ -19,7 +24,7 @@ program.version("0.0.1").description("Crafty CLI")
             .description("Create a new Crafty project")
             .action(async (user, message) => {
                 const postMessageCommand: PostMessageCommand = {
-                    id: "id-message-1",
+                    id: `${Math.floor(Math.random() * 1000000)})}`,
                     text: message,
                     authorId: user,
                 }
@@ -28,6 +33,21 @@ program.version("0.0.1").description("Crafty CLI")
                     console.log("Message posted");
                 } catch (err) {
                     console.error("X", err);
+                }
+            })
+    )
+    .addCommand(
+        new Command("view")
+            .addArgument(new Argument("<user>", "the user to view the messages"))
+            .description("View timeline of a user")
+            .action(async (userId) => {
+                try {
+                    const timeline = await viewTimelineUseCase.handle({userId});
+                    console.table(timeline);
+                    process.exit(0);
+                } catch (err) {
+                    console.error(err);
+                    process.exit(1);
                 }
             })
     );
